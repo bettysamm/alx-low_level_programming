1d5333a078bb77571117106d3608434ac29b89fc
ezzine <ezzineibtissame@gmail.com>
2023-03-10 14:15:09 -0800

    create_static_lib.sh

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,10 +1,9 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+int _atoi(char *s)
 
-void _puts(char *s)
 {
-    puts(s);
+    return atoi(s);
 }
